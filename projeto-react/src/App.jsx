46c7128cc7b87345f7afe5dbb31402506fa15c71
José Alberto <albertofarias07@hotmail.com>
2025-10-07 15:45:00 -0300

    Adiciona a biblioteca uuid e implementa funcionalidade de lista no App

--- projeto-react/src/App.jsx
+++ projeto-react/src/App.jsx
@@ -1,11 +1,40 @@
+import { v4 as uuid } from 'uuid';
+import React, { useState } from 'react';
 
 function App() {
 
+  // useState retorna um array com 2 posições
+  // 1. Variável com o valor do estado = lista
+  // 2. Função para atualizar esse valor = setLista
+  // 3. useState recebe o valor inicial do estado = []
+
+  const [lista, setLista] = useState([{id: uuid(), tarefa: 'Nada'}]);
+
+  function inputChange(event) {
+    console.log(event.target.value);
+    setLista([{id: uuid(), tarefa: event.target.value}])
+
+    console.log(lista);
+  }
+
+  function cliqueiNoBotao() {
+    console.log('Clicou no botao');
+  }
+
   return (
     <div>
-      Olá Mundo!
+
+      <input onChange={inputChange} placeholder="Tarefa para fazer 📝 ... " />
+      <button onClick={cliqueiNoBotao}>Adicionar</button>
+
+      <ul>
+        {lista.map((item) => (
+          <li key={item.id}>{item.tarefa}</li>
+      ))}
+      </ul>
+
     </div>
   )
 }
 
-export default App
+export default App;
