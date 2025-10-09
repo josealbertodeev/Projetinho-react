b7ebfc0da9bfc4eaf634e5ddd81e1fc46d65236f
José Alberto <albertofarias07@hotmail.com>
2025-10-09 15:21:25 -0300

    Atualiza dependências e configurações do projeto

--- projeto-react/src/App.jsx
+++ projeto-react/src/App.jsx
@@ -1,6 +1,7 @@
 import { v4 as uuid } from "uuid";
 import React, { useState } from "react";
-import { Botao, Container, Input, ToDoList } from "./styles";
+import { Botao, Container, Input, ToDoList, ListaItem } from "./styles";
+import { FcEmptyTrash , FcCheckmark } from "react-icons/fc";
 
 function App() {
   // useState retorna um array com 2 posições
@@ -9,7 +10,7 @@ function App() {
   // 3. useState recebe o valor inicial do estado = []
   // uuid é uma função usada para gerar IDs únicos
 
-  const [lista, setLista] = useState([{ id: uuid(), tarefa: "Nada" }]);
+  const [lista, setLista] = useState([{ id: uuid(), tarefa: "Nada", finalizado: true }]);
   const [tarefa, setTarefa] = useState("");
 
   function inputChange(event) {
@@ -17,7 +18,14 @@ function App() {
   }
 
   function cliqueiNoBotao() {
-    setLista([...lista, { id: uuid(), tarefa: tarefa }]);
+    setLista([...lista, { id: uuid(), tarefa: tarefa, finalizado: false }]);
+  }
+
+  function finalizarTarefa(id){
+    const novaLista = lista.map( item => (
+      item.id === id ? { ...item, finalizado: !item.finalizado} : item
+    ))
+    setLista(novaLista)
   }
 
   return (
@@ -32,7 +40,11 @@ function App() {
 
         <ul>
           {lista.map((item) => (
-            <li key={item.id}>{item.tarefa}</li>
+            <ListaItem estaFinalizado={item.finalizado} key={item.id}>
+              <FcCheckmark onClick={() => finalizarTarefa(item.id)} />
+              <li>{item.tarefa}</li>
+              <FcEmptyTrash />
+            </ListaItem>
           ))}
         </ul>
       </ToDoList>
--- projeto-react/src/styles.js
+++ projeto-react/src/styles.js
@@ -16,6 +16,11 @@ export const ToDoList = styled.div`
     background: #FFFFFF;
     border-radius: 5px;
     padding: 30px 20px;
+
+    ul{
+        padding: 0;
+        margin-top: 30px;
+    }
 `
 
 export const Input = styled.input`
@@ -23,6 +28,7 @@ export const Input = styled.input`
     border-radius: 5px;
     height: 40px;
     margin-right: 10px;
+    width: 340px;
 `
 
 export const Botao = styled.button`
@@ -33,4 +39,23 @@ export const Botao = styled.button`
     color: white;
     font-weight: 500;
     height: 40px;
+    width: 130px;
+    cursor: pointer;
+`
+
+export const ListaItem = styled.div`
+    background: ${props => props.estaFinalizado ? '#b0ff8bff' : '#e4e4e4'};
+    box-shadow: 1px 4px 10px rgba(0, 0, 0, 0.2);
+    border-radius: 5px;
+    height: 60px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-bottom: 20px;
+    padding: 0 10px;
+    width: 500px;
+
+    li{
+        list-style: none;
+    }
 `
